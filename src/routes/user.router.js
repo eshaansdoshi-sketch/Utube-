6c8721ce7bb14ebe400d65e02287ffdee3736dbc
eshaansdoshi-sketch <eshaansdoshi@gmail.com>
2026-01-25 16:26:21 +0530

    Add new routes for user controller

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccesToken} from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccesToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 
 const router = Router()
 
@@ -18,13 +19,21 @@ router.route("/register").post(
     ]),
     registerUser
 )
-
 router.route("/login").post(loginUser)
 
 // Secured routes
 
 router.route("/logout").post(verifyJWT, logoutUser)
-
 router.route("/refresh-token").post(refreshAccesToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload. single('avatar'), updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile) // its a specific pattern to follow when u r getting data from url(req.params)
+                                                                         //  and the name of what u r taking should also match with ethat was defined in the controller
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
